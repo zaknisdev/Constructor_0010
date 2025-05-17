ae14d6a13a6f945a05e3fdf84fffbee3a003ff89
zaknisdev <zakianisf1710@gmail.com>
2025-05-17 13:52:18 +0700

    membuat fungsi status buku

--- Constructor.cpp
+++ Constructor.cpp
@@ -7,5 +7,12 @@ class Buku {
         string judul;
         string penulis;
         bool dipinjam;
+
+        public:
+    Buku(string j, string p) : judul(j), penulis(p), dipinjam(false) {}
+
+    void status() {
+        cout << "Buku: " << judul << " oleh " << penulis << (dipinjam ? " [Dipinjam]" : " [Tersedia]") << endl;
+        }
     };
     
