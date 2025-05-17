4711db2e5b58ba8e4f7c0eefcc57f5a129e3ee0d
zaknisdev <zakianisf1710@gmail.com>
2025-05-17 14:17:23 +0700

    chain function

---
+++ chainfunction.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class buku {
+    string judul;
+
+public:
+    buku setJudul(string judul) {
+        this->judul = judul;
+        return *this; // chain function
+    }
+
+    string getJudul() {
+        return this->judul;
+    }
+} bukunya;
+
+int main() {
+    // bukunya.setJudul("Matematika");
+    // cout << bukunya.getJudul();
+    cout << bukunya.setJudul("Matematika").getJudul(); // chain function calls
+    return 0;
+}
